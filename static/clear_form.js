2761586d72f5ad373cae5e1b6e437be2eeaf3a68
Carlos Gomez <CGomez374@yahoo.com>
2023-07-03 13:54:58 -0700

    fixed landscape styling

--- static/clear_form.js
+++ static/clear_form.js
@@ -1,6 +1,6 @@
 const clear_form = () => {
     var input_elements = document.getElementsByTagName("input");
-    var new_textarea_element = document.getElementsByID("new_review_content");
+//    var new_textarea_element = document.getElementsByID("new_review_content");
     for (var ii=0; ii < input_elements.length; ii++) {
         if (input_elements[ii].type == "text" || input_elements[ii].type == "email" || input_elements[ii].type == "password") {
             input_elements[ii].value = "";
